ba9f0b57cf6386b0701cb045d407918442346394
louge <louge1010@gmail.com>
2024-01-13 00:55:38 +0800

    first commit

---
+++ src/main/java/com/louge/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.louge.git;
+
+/**
+ * @program: git-test
+ * @description:
+ * @author: louge1010@gmail.com
+ * @create: 2024-01-13 00:50
+ **/
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
